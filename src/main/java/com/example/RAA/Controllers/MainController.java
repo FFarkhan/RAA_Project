7c360db7e8cb48242701efd175bdfb8890a11865
farkhan <fazilli_farxan@mail.ru>
2024-02-25 01:52:04 +0400

    Initial commit

---
+++ src/main/java/com/example/RAA/Controllers/MainController.java
@@ -0,0 +1,13 @@
+package com.example.RAA.Controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+    @GetMapping("/")
+    public String main_page(Model model){
+        return "main-page";
+    }
+}
---
+++ src/main/java/com/example/RAA/RaaApplication.java
@@ -0,0 +1,13 @@
+package com.example.RAA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RaaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RaaApplication.class, args);
+	}
+
+}
