434e3db882275e88848fa6a089d2f50ebc81635d
farkhan <fazilli_farxan@mail.ru>
2024-03-02 19:38:38 +0400

    Project Upgrade

--- src/main/java/com/example/RAA/Controllers/MainController.java
+++ src/main/java/com/example/RAA/Controllers/MainController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
-    @GetMapping("/")
+    @GetMapping("/main")
     public String main_page(Model model){
         return "main-page";
     }
